2999191194c0211fe61746b6879bb326d3d175d8
7Luke7 <pro.chikvaidze@gmail.com>
2023-11-07 18:41:23 +0400

    Update Sidebar.js

--- client/app/chat/sidebar/Sidebar.js
+++ client/app/chat/sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 "use client"
+import Image from 'next/image'
 import { FriendsContext } from '../chatcontext'
 import { useContext } from 'react'
 
@@ -10,7 +11,7 @@ const Sidebar = ({currentUser, save_user_to_session}) => {
 
                         <div className="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
                             <div>
-                                <img className="w-10 h-10 rounded-full" src="https://darrenjameseeley.files.wordpress.com/2014/09/expendables3.jpeg"/>
+                                <Image alt="seome" className="w-10 h-10 rounded-full" src="https://darrenjameseeley.files.wordpress.com/2014/09/expendables3.jpeg"/>
                             </div>
                         </div>
 
@@ -23,7 +24,7 @@ const Sidebar = ({currentUser, save_user_to_session}) => {
                             {friends.map((f, i) => {
                                 return <div key={i} onClick={() => save_user_to_session(f)} className={`px-3 border-b ${!currentUser.userId && JSON.parse(sessionStorage.getItem("ucw"))?.userId === f.id ? "bg-gray-200" : currentUser.userId === f.id ? "bg-gray-200" : ""} border-grey-lighter flex items-center bg-grey-light`}>
                                 <div>
-                                    <img className="h-12 w-12 rounded-full"
+                                    <Image className="h-12 w-12 rounded-full" alt="some"
                                          src="https://darrenjameseeley.files.wordpress.com/2014/09/expendables3.jpeg"/>
                                 </div>
                                 <div className="ml-4 flex-1 py-4">
